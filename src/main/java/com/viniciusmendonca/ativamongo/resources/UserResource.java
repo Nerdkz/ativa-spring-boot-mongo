bb2e58884c629edd31052f5eb89e8a858368acd4
Nerdkz <viniciusmendonca@gec.inatel.br>
2021-07-05 14:28:52 -0300

    Conectando ao MongoDB com Repository e Service

--- src/main/java/com/viniciusmendonca/ativamongo/resources/UserResource.java
+++ src/main/java/com/viniciusmendonca/ativamongo/resources/UserResource.java
@@ -4,24 +4,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.viniciusmendonca.ativamongo.domain.User;
+import com.viniciusmendonca.ativamongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity <List<User>> findAll() {
-		User vinicius = new User("1", "Vinicius", "vinicius@hotmail.com", "123456");
-		User alex = new User("2", "Alex", "alex@hotmail.com", "123456");
+		//User vinicius = new User("1", "Vinicius", "vinicius@hotmail.com", "123456");
+		//User alex = new User("2", "Alex", "alex@hotmail.com", "123456");
 		
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(vinicius, alex));
+		List<User> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
