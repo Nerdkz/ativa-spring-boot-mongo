586eedae55cedaf0ccea4040fedf04b293116013
Nerdkz <viniciusmendonca@gec.inatel.br>
2021-07-05 11:39:46 -0300

    firt commit

---
+++ src/main/java/com/viniciusmendonca/ativamongo/AtivamongoApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusmendonca.ativamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtivamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtivamongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusmendonca/ativamongo/AtivamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusmendonca.ativamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtivamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
