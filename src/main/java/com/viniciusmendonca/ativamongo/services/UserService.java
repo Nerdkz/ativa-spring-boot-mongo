0392cb085cbce150370280de6801f7379950295e
Nerdkz <viniciusmendonca@gec.inatel.br>
2021-07-05 15:38:56 -0300

    Inserção de Usuário com POST

--- src/main/java/com/viniciusmendonca/ativamongo/services/UserService.java
+++ src/main/java/com/viniciusmendonca/ativamongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.viniciusmendonca.ativamongo.domain.User;
+import com.viniciusmendonca.ativamongo.dto.UserDTO;
 import com.viniciusmendonca.ativamongo.repository.UserRepository;
 import com.viniciusmendonca.ativamongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail(), objDto.getPassword());
 	}
 }
