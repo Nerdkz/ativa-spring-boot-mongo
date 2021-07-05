7d1b622dd3a0658b7c11cf2dc9290a5aa78a4724
Nerdkz <viniciusmendonca@gec.inatel.br>
2021-07-05 19:50:57 -0300

    Atualização de usuário com PUT

--- src/main/java/com/viniciusmendonca/ativamongo/resources/UserResource.java
+++ src/main/java/com/viniciusmendonca/ativamongo/resources/UserResource.java
@@ -52,6 +52,7 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	
 	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
 	public ResponseEntity <Void> delete(@PathVariable String id) {
 		
@@ -60,4 +61,13 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value= "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
 }
